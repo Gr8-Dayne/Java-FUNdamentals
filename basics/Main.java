da5e15cb60a2543345c06fd4596383c3c4c5749e
Dayne Daylong <dalexad@gmail.com>
2020-01-07 12:09:42 -0800

    finished first paramater of Lab

---
+++ basics/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int dogCount = 1;
+    System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+    int catCount = 2;
+    System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+    int turtleCount = 0;
+    System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
+
+  }
+
+  public static String pluralize (String word, int number) {
+    if (number == 1) {
+      return word;
+    } else {
+      return word + "s";
+    }
+  }
+
+}
+
+// public class ExampleSyntax {
+//   public static void main(String[] args) {
+//     String greeting = "Welcome to my Java program.";
+//     System.out.println(greeting);
+
+//     int x = 7;
+//     float f = 42.3f;
+//     double d = 42.3;
+
+//     // execute the function called "loopTo" and pass 10 as a parameter
+//     loopTo(10);
+
+//     int four = 4;
+//     int result = multiplyByTwo(four);
+//     System.out.println(four + " doubled is " + result);
+//   }
+
+//   // Functions that return nothing have a return type of "void"
+//   public static void loopTo(int max) {
+//     for (int i = 0; i < max; i++) {
+//       System.out.println("Loop #" + i);
+//     }
+//   }
+
+//   // This function has a return type of "int"
+//   public static int multiplyByTwo(int n) {
+//     return n * 2;
+//   }
+// }
